9e4c9527406eb499b3dced92f7756b8ba7c155f4
pablito223421 <pablohernandezhernandezuacm@gmail.com>
2022-10-07 20:57:08 -0500

    Programa sencillo en Vue

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+data:() => ({
+  contador:0
+}),
+methods: {
+   incrementar(){
+     this.contador++;
+   },
+   disminuir(){
+     this.contador--;
+   },
+   reset(){
+    this.contador=0;
+   }
+},
+};
+</script>
+
+<template> 
+<div class="contaniner">
+<img src="./assets/logo.svg" alt="logo vue" width="125" height="125">
+<small>
+  <h1>Hola Mundo desde Vue</h1>
+  </small>
+<hr />
+
+<h2>Contador: {{contador}}</h2>
+<button @click="incrementar()">+1</button>
+<button @click="disminuir()">-1</button>
+<button @click="reset()">Reset</button>
+</div>
+</template>
+
+<style scoped>
+.container{
+text-align: center;
+}
+
+small{
+  color:#666;
+}
+
+h1{
+ font-weight: bolder;
+}
+
+h2{
+  font-size: 2.5rem;
+}
+
+button{
+  cursor: pointer;
+  font-size: 1.5rem;
+  margin: 0px 6px;
+}
+
+</style>
